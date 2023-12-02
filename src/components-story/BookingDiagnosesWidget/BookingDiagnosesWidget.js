5982064e9db4dd3fb67215951f9c1e2fe5dfbf78
Marcin Kula <marcin@chicos.life>
2023-12-02 16:40:48 +0100

    Fix bug that does not allow storyblok to recognize component in visual editor

--- src/components-story/BookingDiagnosesWidget/BookingDiagnosesWidget.js
+++ src/components-story/BookingDiagnosesWidget/BookingDiagnosesWidget.js
@@ -3,10 +3,12 @@ import '../../components-story/BookingDiagnosesWidget/booking.css';
 
 const BookingDiagnosesWidget = () => {
   return (
-    <iframe
-      className='diagnoses-booking-widget'
-      src='https://widget.zarezerwuj.pl/757f51d8-6b42-4449-8379-9f99d36367bb'
-    />
+    <div className='booking-widget-wrapper'>
+      <iframe
+        className='diagnoses-booking-widget'
+        src='https://widget.zarezerwuj.pl/757f51d8-6b42-4449-8379-9f99d36367bb'
+      />
+    </div>
   );
 };
 
